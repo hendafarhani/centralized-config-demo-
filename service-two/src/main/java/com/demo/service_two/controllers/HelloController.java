25900a74a79390e244d9c387fd215d0becfe22f1
henda <farhanii.henda@gmail.com>
2025-06-07 16:37:20 +0200

    Add Dicovery server Add controllers service-one and service-two

---
+++ service-two/src/main/java/com/demo/service_two/controllers/HelloController.java
@@ -0,0 +1,16 @@
+package com.demo.service_two.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Value;
+
+@RestController
+public class HelloController {
+    @Value("${message}")
+    private String message;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return message;
+    }
+}
